374b5de69cfe6402049dbd3c5ef0c45b98a4dd98
Samuel Ospina <samiospina2006@gmail.com>
2025-12-05 23:15:13 -0500

    VideoLlamada e Historial Medico

--- medconnect/src/app/features/historial/historial/historial.ts
+++ medconnect/src/app/features/historial/historial/historial.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-historial',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './historial.html',
   styleUrls: ['./historial.scss'],
 })
 export class Historial {
+    // Datos quemados (luego los puedes conectar a la base de datos)
+  nombre = "Samuel Urbano Millan";
+  cedula = "1108642121";
+  registro = "123";
+
+  motivoConsulta = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+  ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+  in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`
 
 }
--- medconnect/src/app/features/videollamada/videocall/videocall.ts
+++ medconnect/src/app/features/videollamada/videocall/videocall.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-videocall',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgClass],
   templateUrl: './videocall.html',
   styleUrls: ['./videocall.scss'],
 })
 export class Videocall {
+  muted = false;
 
+  constructor (private router: Router) {}
+
+  toggleMute() {
+    this.muted = !this.muted;
+  }
+
+  colgar() {
+    this.router.navigate(['/historial']);
+  }
 }
