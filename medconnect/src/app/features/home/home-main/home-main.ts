4bc663ed472ef430ec56c07ad6e759e6fa34ceb0
luiseurban <165191210+luiseurban@users.noreply.github.com>
2025-12-06 00:24:08 -0500

    Manejo de sesion en el Home

--- medconnect/src/app/features/home/home-main/home-main.ts
+++ medconnect/src/app/features/home/home-main/home-main.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
@@ -6,10 +7,15 @@ import { FooterComponent } from '../../../shared/components/footer/footer.compon
 @Component({
   selector: 'app-home-main',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterModule],
+  imports: [HeaderComponent, FooterComponent, CommonModule, RouterModule],
   templateUrl: './home-main.html',
   styleUrls: ['./home-main.scss'],
 })
 export class HomeMain {
-
+  get isLoggedIn(): boolean {
+    if (typeof window === 'undefined' || typeof window.localStorage === 'undefined') {
+      return false;
+    }
+    return !!window.localStorage.getItem('medconnect_current_user');
+  }
 }
